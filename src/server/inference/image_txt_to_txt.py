c12b51aed128fca0781c137dbcf7e554dcffe5dc
Sheldon Bish <sbish33@gmail.com>
2026-03-24 16:09:20 -0400

    Image text to txt works

--- src/server/inference/image_txt_to_txt.py
+++ src/server/inference/image_txt_to_txt.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 from vllm import LLM, SamplingParams
 from PIL import Image
 from pdf2image import convert_from_path
+from transformers import AutoTokenizer
 
 load_dotenv()
 OCR_DIR = os.getenv("OCR_MODEL_STORAGE")
@@ -13,13 +14,14 @@ class smart_ocr():
         # self.model_name = model_name
         # It is recommended to use the tokenizer from the base (non-GGUF) model 
         # for stability and performance.
-        llm = LLM(
-            model=f"{OCR_DIR}Qwen/Qwen3-VL-4B-Instruct/1",
-            tokenizer="Qwen/Qwen3-VL-4B-Instruct",
-            hf_config_path="Qwen/Qwen3-VL-4B-Instruct",
-            gpu_memory_utilization=0.4, 
-            trust_remote_code=True,
-            max_model_len=4096
+        self.llm = LLM(
+            model=f"{OCR_DIR}",
+            tokenizer=f"{OCR_DIR}",
+            hf_config_path=f"{OCR_DIR}",
+            gpu_memory_utilization=0.8, 
+            # enforce_eager=True    # Use if memory is tight
+            max_model_len=4096,
+            max_cudagraph_capture_size=4,
         )
         # Resolution            |	Calculation	Estimated Tokens
         # 512 × 512	            |  256 tokens
@@ -41,23 +43,37 @@ class smart_ocr():
             print(f"Extension {extension} is not recognized.")
 
     def inference(self, 
+            image: str,
             prompt_str: str = "Describe this image in detail.",
-            verbose: bool = False,
-            image: str = image_dir,):
+            verbose: bool = False):
         # Prepare a multimodal prompt (text + image)
-        self.decode_image(image_dir: str)
-        prompt = {
-            "prompt": prompt_str,
-            "multi_modal_data": {
-                "image": self.image 
-            }
+        self.decode_image(image)
+
+        tokenizer = AutoTokenizer.from_pretrained(OCR_DIR, trust_remote_code=True)
+        messages = [
+                {
+                    "role": "user",
+                    "content": [
+                        {"type": "image"},
+                        {"type": "text", "text": prompt_str},
+                    ],
+                }
+            ]
+
+        prompt = tokenizer.apply_chat_template(
+            messages, 
+            tokenize=False, 
+            add_generation_prompt=True
+        )
+        inputs = {
+            "prompt": prompt,
+            "multi_modal_data": {"image": self.image}
         }
         # Batching: If you want to process multiple images in a single prompt 
         # (if supported by your specific vLLM version), you would pass a list 
         # of these objects: {"image": [image1, image2]}.
-
         sampling_params = SamplingParams(temperature=0.2, max_tokens=512)
-        outputs = llm.generate([prompt], sampling_params=sampling_params)
+        outputs = self.llm.generate([inputs], sampling_params=sampling_params)
 
         for output in outputs:
             for completion in output.outputs:
